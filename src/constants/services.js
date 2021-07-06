f32fd568c27fbc592a1a0009073813cbef203fa8
Roberto <rob.len001@gmail.com>
2021-07-06 19:36:20 -0400

    small changes

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,7 +5,7 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I have expirience in coding, designing and testing websites. I utilize modern web development tools to produce cutting edge and creative websites.`,
+    text: `I have experience in coding, designing and testing websites. I utilize modern web development tools to produce cutting edge and creative websites.`,
   },
   {
     id: 2,
@@ -17,6 +17,6 @@ export default [
     id: 3,
     icon: <FaAndroid className="service-icon" />,
     title: "data analysis",
-    text: `I have extensive expirience in both utilizing and creating software to facilitate the process of cleaning, organizing and modeling data.`,
+    text: `I have extensive experience in both utilizing and creating software to facilitate the process of cleaning, organizing and modeling data.`,
   },
 ]
